a928dc465f613ad9343e9b4958e232090a0535e4
Ilya Melnikov <ilyamelhik@yandex.ru>
2025-04-23 18:46:03 +0500

    create model Product

--- src/main/java/Study/SpringBootWithoutLombok/controllers/ProductController.java
+++ src/main/java/Study/SpringBootWithoutLombok/controllers/ProductController.java
@@ -1,4 +1,4 @@
-package Study.SpringBootWithoutLombok.controller;
+package Study.SpringBootWithoutLombok.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/Study/SpringBootWithoutLombok/models/Product.java
@@ -0,0 +1,9 @@
+package Study.SpringBootWithoutLombok.models;
+
+public class Product {
+    private String title;
+    private String description;
+    private int price;
+    private String city;
+    private String author;
+}
