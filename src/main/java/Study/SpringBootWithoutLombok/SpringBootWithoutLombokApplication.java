6bffcf4c4b5c5bb78462f2e67d98017b802c9c23
Ilya Melnikov <ilyamelhik@yandex.ru>
2025-04-23 18:12:08 +0500

    init commit

---
+++ src/main/java/Study/SpringBootWithoutLombok/SpringBootWithoutLombokApplication.java
@@ -0,0 +1,13 @@
+package Study.SpringBootWithoutLombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithoutLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithoutLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Study/SpringBootWithoutLombok/SpringBootWithoutLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package Study.SpringBootWithoutLombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithoutLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
